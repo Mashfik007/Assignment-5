6c9a5e3ba2ef610eefbfe2645ae4ab2e8a49eff6
Mashfik <163512654+Mashfik007@users.noreply.github.com>
2024-09-23 23:31:28 +0600

    done

--- index.js
+++ index.js
@@ -1,56 +1,85 @@
-'use strict'
+"use strict";
 document.querySelector("#history").addEventListener("click", function () {
   window.location.href = "./history.html";
   this.style.backgroundColor = "#a3e635";
-  
- 
 });
-let donation = parseInt(document.querySelector('#cal').innerHTML)
-let reduce = parseInt(document.querySelector('#reduce').innerHTML)
-let sum1,sum2;
 
+let perdiv = [];
 
-document.querySelector("#Close").addEventListener('click', function () {
-  document.querySelector("#congrats").style.display='none';
+function total(donation, donation2) {
+  let sum = donation + donation2;
+  console.log(sum);
+  return sum;
+}
 
+let donation = parseInt(document.querySelector("#cal").innerHTML);
+let donation2 = parseInt(document.querySelector("#cal2").innerHTML);
+let reduce = parseInt(document.querySelector("#reduce").innerHTML);
 
-})
-
-document.querySelector('#button1').addEventListener('click', function () {
-  let n = parseInt(document.querySelector('#input1').value);
+document.querySelector("#Close").addEventListener("click", function () {
+  document.querySelector("#congrats").style.display = "none";
+});
+// 1st donation button
+document.querySelector("#button1").addEventListener("click", function () {
+  let n = parseInt(document.querySelector("#input1").value);
 
   if (isNaN(n) || n < 0) {
-    alert('its not a number')
+    alert("its not a number");
   } else {
+    const date = new Date()
+      .toLocaleDateString("default", {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "numeric",
+        hour12:true
+      })
+      .toLocaleUpperCase();
+    // console.log(date);
 
-   sum1=parseInt(document.querySelector('#cal').innerHTML = `${donation += n}`)
-    document.querySelector('#reduce').innerHTML = `${reduce -= n}`
-    // console.log(nowakhli_donation);
-    document.querySelector("#congrats").style.display = 'flex'
-
+    donation += n;
+    document.querySelector("#cal").innerHTML = `${donation}`;
+    document.querySelector("#reduce").innerHTML = `${(reduce -= n)}`;
+    document.querySelector("#congrats").style.display = "flex";
+    perdiv.push({
+      amount: n,
+      place: "Flood Relief in Feni",
+      date: date,
+    });
+    console.log(perdiv);
   }
-
-
-})
-
+});
 // 2nd
-document.querySelector('#button2').addEventListener('click', function () {
-  let n = parseInt(document.querySelector('#input2').value);
+document.querySelector("#button2").addEventListener("click", function () {
+  let n = parseInt(document.querySelector("#input2").value);
 
   if (isNaN(n) || n < 0) {
-    alert('its not a number')
+    alert("its not a number");
   } else {
+    const date = new Date()
+      .toLocaleDateString("default", {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "numeric",
+      })
+      .toLocaleUpperCase();
 
-   sum2= parseInt(document.querySelector('#cal2').innerHTML = `${donation += n}`)
-    document.querySelector('#reduce').innerHTML = `${reduce -= n}`
-    document.querySelector("#congrats").style.display = 'flex'
-       
+    perdiv.push({
+      amount: n,
+      place: "flood at Nowakhali",
+      date: date,
+    });
+    console.log(perdiv);
+    donation2 += n;
+    document.querySelector("#cal2").innerHTML = `${donation2}`;
+    document.querySelector("#reduce").innerHTML = `${(reduce -= n)}`;
+    document.querySelector("#congrats").style.display = "flex";
+
+    total(donation, donation2);
   }
-
-  console.log(sum1+sum2);
-  
-
-
-})
-
-
+});
