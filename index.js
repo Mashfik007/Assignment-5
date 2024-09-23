abd2bbc073e6904ab6bf8615f84b07fbc593398f
Mashfik <163512654+Mashfik007@users.noreply.github.com>
2024-09-23 19:11:21 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 document.querySelector("#history").addEventListener("click", function () {
-  window.location.href = "/history.html";
+  window.location.href = "./history.html";
   this.style.backgroundColor = "#a3e635";
 });
 let donation = parseInt(document.querySelector('#cal').innerHTML)
@@ -19,8 +19,9 @@ document.querySelector('#button1').addEventListener('click', function () {
     document.querySelector('#reduce').innerHTML = `${reduce-=n}`
     // console.log(nowakhli_donation);
     document.querySelector("#congrats").style.display='flex'
-    document.querySelector("#close").addEventListener('click',function () {
-    document.querySelector("#congrats").remove;
+    document.querySelector("#Close").addEventListener('click',function () {
+    document.querySelector("#congrats").remove();
+
 
     })
   }
@@ -29,18 +30,4 @@ document.querySelector('#button1').addEventListener('click', function () {
 })
 
 // 2nd
-document.querySelector('#button2').addEventListener('click', function () {
-  let n = parseInt(document.querySelector('#input2').value);
 
-  if (isNaN(n) || n < 0) {
-    alert('its not a number')
-  } else {
-   
-    document.querySelector('#cal2').innerHTML =`${donation+=n}`
-    document.querySelector('#reduce').innerHTML = `${reduce-=n}`
-    // console.log(nowakhli_donation);
-
-  }
-
-
-})
