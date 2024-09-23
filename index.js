f86956ab2eb37249fc3320c58e6f6e4a16b9fba1
Mashfik <163512654+Mashfik007@users.noreply.github.com>
2024-09-23 17:44:12 +0600

    counting done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.querySelector("#history").addEventListener("click", function () {
   window.location.href = "/history.html";
   this.style.backgroundColor = "#a3e635";
 });
-let nowakhli_donation = parseInt(document.querySelector('#cal').innerHTML)
+let donation = parseInt(document.querySelector('#cal').innerHTML)
 let reduce = parseInt(document.querySelector('#reduce').innerHTML)
 
 
@@ -15,7 +15,7 @@ document.querySelector('#button1').addEventListener('click', function () {
     alert('its not a number')
   } else {
    
-    document.querySelector('#cal').innerHTML =`${ nowakhli_donation+=n}`
+    document.querySelector('#cal').innerHTML =`${donation+=n}`
     document.querySelector('#reduce').innerHTML = `${reduce-=n}`
     // console.log(nowakhli_donation);
 
@@ -23,14 +23,16 @@ document.querySelector('#button1').addEventListener('click', function () {
 
 
 })
-document.querySelector('#button1').addEventListener('click', function () {
-  let n = parseInt(document.querySelector('#input1').value);
+
+// 2nd
+document.querySelector('#button2').addEventListener('click', function () {
+  let n = parseInt(document.querySelector('#input2').value);
 
   if (isNaN(n) || n < 0) {
     alert('its not a number')
   } else {
    
-    document.querySelector('#cal').innerHTML =`${ nowakhli_donation+=n}`
+    document.querySelector('#cal2').innerHTML =`${donation+=n}`
     document.querySelector('#reduce').innerHTML = `${reduce-=n}`
     // console.log(nowakhli_donation);
 
