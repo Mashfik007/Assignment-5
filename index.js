257325a794109a58fd8805e833925e2eaea76280
Mashfik <163512654+Mashfik007@users.noreply.github.com>
2024-09-23 19:55:32 +0600

    update

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 document.querySelector("#history").addEventListener("click", function () {
   window.location.href = "./history.html";
   this.style.backgroundColor = "#a3e635";
+  
+ 
 });
 let donation = parseInt(document.querySelector('#cal').innerHTML)
 let reduce = parseInt(document.querySelector('#reduce').innerHTML)
+let sum1,sum2;
 
 
+document.querySelector("#Close").addEventListener('click', function () {
+  document.querySelector("#congrats").style.display='none';
+
+
+})
 
 document.querySelector('#button1').addEventListener('click', function () {
   let n = parseInt(document.querySelector('#input1').value);
@@ -14,16 +22,12 @@ document.querySelector('#button1').addEventListener('click', function () {
   if (isNaN(n) || n < 0) {
     alert('its not a number')
   } else {
-     
-    document.querySelector('#cal').innerHTML =`${donation+=n}`
-    document.querySelector('#reduce').innerHTML = `${reduce-=n}`
+
+   sum1=parseInt(document.querySelector('#cal').innerHTML = `${donation += n}`)
+    document.querySelector('#reduce').innerHTML = `${reduce -= n}`
     // console.log(nowakhli_donation);
-    document.querySelector("#congrats").style.display='flex'
-    document.querySelector("#Close").addEventListener('click',function () {
-    document.querySelector("#congrats").remove();
+    document.querySelector("#congrats").style.display = 'flex'
 
-
-    })
   }
 
 
@@ -36,16 +40,15 @@ document.querySelector('#button2').addEventListener('click', function () {
   if (isNaN(n) || n < 0) {
     alert('its not a number')
   } else {
-     
-    document.querySelector('#cal2').innerHTML =`${donation+=n}`
-    document.querySelector('#reduce').innerHTML = `${reduce-=n}`
-    document.querySelector("#congrats").style.display='flex'
-    document.querySelector("#Close").addEventListener('click',function () {
-    document.querySelector("#congrats").remove();
 
-
-    })
+   sum2= parseInt(document.querySelector('#cal2').innerHTML = `${donation += n}`)
+    document.querySelector('#reduce').innerHTML = `${reduce -= n}`
+    document.querySelector("#congrats").style.display = 'flex'
+       
   }
+
+  console.log(sum1+sum2);
+  
 
 
 })
